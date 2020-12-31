11e8c22ad78bf56d1bcfa030de6d8abcdbe52f82
Alex Lungu <l.alex1999@yahoo.com>
2020-12-31 19:19:15 +0200

    Sect 3

---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductByIdAsync(int id);
+        Task<IReadOnlyList<Product>> getProductsAsync();
+        Task<IReadOnlyList<ProductBrand>> getProductBrandsAsync();
+        Task<IReadOnlyList<ProductType>> getProductTypesAsync();
+    }
+}
