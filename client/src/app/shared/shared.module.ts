e8b533e3ed5a3fb8394b067bcc3ae552192a8516
Alex Lungu <l.alex1999@yahoo.com>
2021-01-11 14:21:08 +0200

    Add Checkout to UI

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { CarouselModule, PaginationModule } from 'ngx-bootstrap';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 import { PagerComponent } from './components/pager/pager.component';
+import { OrderTotalsComponent } from './components/order-totals/order-totals.component';
 
 @NgModule({
-  declarations: [PagingHeaderComponent, PagerComponent],
+  declarations: [PagingHeaderComponent, PagerComponent, OrderTotalsComponent],
   imports: [
     CommonModule,
     PaginationModule.forRoot(),
@@ -16,6 +17,7 @@ import { PagerComponent } from './components/pager/pager.component';
     PagingHeaderComponent,
     PagerComponent,
     CarouselModule,
+    OrderTotalsComponent,
   ]
 })
 export class SharedModule { }
